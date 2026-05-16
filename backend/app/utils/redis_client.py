4e7c83e84d06eaf526d3c44e51b129e90ec097a6
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-05-16 14:14:03 +0530

    Connecting redis to server

---
+++ backend/app/utils/redis_client.py
@@ -0,0 +1,15 @@
+import redis
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+redis_client = redis.from_url(
+    os.getenv("REDIS_URL"),
+    decode_responses=True
+)
+
+redis_client.ping()
+
+print("Redis connected")
