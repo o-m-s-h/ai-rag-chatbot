4e6d8f0d2840e64b2a0679808f923fdfb2c4de52
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-05-16 14:06:49 +0530

    Local -> cloud

--- backend/app/database/chroma_db.py
+++ backend/app/database/chroma_db.py
@@ -1,7 +1,16 @@
 import chromadb
 
-client = chromadb.PersistentClient(
-    path="chroma_storage"
+from chromadb.config import Settings
+
+from app.core.config import settings
+
+client = chromadb.CloudClient(
+
+    api_key=settings.CHROMA_API_KEY,
+
+    tenant=settings.CHROMA_TENANT,
+
+    database=settings.CHROMA_DATABASE
 )
 
 collection = client.get_or_create_collection(
